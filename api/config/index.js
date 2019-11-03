785fa211738658dd2e6d55833c02e94d5195c961
eyob tariku <eyobtariku@gmail.com>
2019-11-03 11:57:09 +0300

    Change the mongodb url to read from env

--- api/config/index.js
+++ api/config/index.js
@@ -3,7 +3,7 @@ module.exports = {
   PORT: process.env.PORT || 3000 ,
   // MongoDB URL
   // MONGOLAB_URI: process.env.MONGOLAB_URI || "mongodb://10.142.0.15:27017/internAt",
-  MONGOLAB_URI: "mongodb://10.142.0.15:27017/internAt",
+  MONGOLAB_URI: process.env.MONGOLAB_URI,
   // JWT Key
   JWT_KEY: "HelloInternAtSecret"
 };
