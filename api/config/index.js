75c22860ec42e2b4397c7aecb075ced251f16136
eyob tariku <eyobtariku@gmail.com>
2019-11-03 11:50:05 +0300

    Debug the mongodb url

--- api/config/index.js
+++ api/config/index.js
@@ -2,7 +2,8 @@ module.exports = {
   // HTTP PORT
   PORT: process.env.PORT || 3000 ,
   // MongoDB URL
-  MONGOLAB_URI: process.env.MONGOLAB_URI || "mongodb://10.142.0.15:27017/internAt",
+  // MONGOLAB_URI: process.env.MONGOLAB_URI || "mongodb://10.142.0.15:27017/internAt",
+  MONGOLAB_URI: "mongodb://10.142.0.15:27017/internAt",
   // JWT Key
   JWT_KEY: "HelloInternAtSecret"
 };
