44fb45474a35bbb9b432adde8132dee8518a71e2
Kidist-Abraham <kidistabraham@gmail.com>
2019-11-06 11:09:05 +0300

    Fix createing new user on update

--- api/controllers/adminController.js
+++ api/controllers/adminController.js
@@ -19,7 +19,7 @@ exports.get_admin = function(req, res) {
 
 
 exports.update_admin = function(req, res) {
-  Admin.findOneAndUpdate({_id: req.params.adminId}, req.body, {new: true}, function(err, admin) {
+  Admin.findOneAndUpdate({_id: req.params.adminId}, req.body, function(err, admin) {
     if (err){
       return  res.json({success:false,
                     err:err}); }
